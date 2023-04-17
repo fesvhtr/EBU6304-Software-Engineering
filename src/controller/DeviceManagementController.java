cad34f3f301e835dee25bf2bea165fbb58e8b32b
Xian Zhang <zhangxian2020@bupt.edu.cn>
2023-04-18 05:46:28 +0800

    Activity finished
    todo: Alerts, date

--- src/controller/DeviceManagementController.java
+++ src/controller/DeviceManagementController.java
@@ -143,14 +143,17 @@ public class DeviceManagementController implements Initializable {
             deviceObservableList.add(d);
         }
         table.setItems(deviceObservableList);
+//        idCol.setCellValueFactory(new PropertyValueFactory<Device, String>("id"));
+//        nameCol.setCellValueFactory(new PropertyValueFactory<Device, String>("name"));
+//        typeCol.setCellValueFactory(new PropertyValueFactory<Device, String>("type"));
+//        specCol.setCellValueFactory(new PropertyValueFactory<Device, String>("spec"));
+//        descriptionCol.setCellValueFactory(new PropertyValueFactory<Device, String>("description"));
+//        statusCol.setCellValueFactory(new PropertyValueFactory<Device, String>("status"));
+//        rentStatusCol.setCellValueFactory(new PropertyValueFactory<Device, String>("rentStatus"));
+//        userCol.setCellValueFactory(new PropertyValueFactory<Device, String>("user"));
         idCol.setCellValueFactory(new PropertyValueFactory<Device, String>("id"));
         nameCol.setCellValueFactory(new PropertyValueFactory<Device, String>("name"));
         typeCol.setCellValueFactory(new PropertyValueFactory<Device, String>("type"));
-        specCol.setCellValueFactory(new PropertyValueFactory<Device, String>("spec"));
-        descriptionCol.setCellValueFactory(new PropertyValueFactory<Device, String>("description"));
-        statusCol.setCellValueFactory(new PropertyValueFactory<Device, String>("status"));
-        rentStatusCol.setCellValueFactory(new PropertyValueFactory<Device, String>("rentStatus"));
-        userCol.setCellValueFactory(new PropertyValueFactory<Device, String>("user"));
 
     }
 
